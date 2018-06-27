d91d40fdf15ef8ad5b2eb3505ea150e06e8989dd
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-27 09:02:39 -0700

    coded add item functionlities

--- js/app.js
+++ js/app.js
@@ -26,6 +26,25 @@ const ItemCtrl = (function() {
             return data.items;
         },
 
+        addItem: function(name, calories) {
+            // Generate ID
+            let ID;
+            if ( data.items.length > 0 ) {
+                ID = data.items[data.items.length-1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+            // Calories to number
+            calories = parseInt(calories);
+            const newItem = new Item(ID, name, calories);
+            
+            // Add to Items array
+            data.items.push(newItem);
+
+            return newItem;
+        },
+
         logData: function() {
             return data;
         }
@@ -34,11 +53,19 @@ const ItemCtrl = (function() {
 
 // UI Ctrl
 const UICtrl = (function() {
+
+    // 
+    const UISelectors = {
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        nameInput: '#item-name',
+        caloriesInput: '#item-calories'
+    };
     
     // PUBLIC METHODS
     return {
         populateItemList: function(items) {
-            const listContainer = document.querySelector('#item-list');
+            const listContainer = document.querySelector(UISelectors.itemList);
             let html = '';
 
             items.forEach( (item) => {
@@ -46,18 +73,71 @@ const UICtrl = (function() {
                 <li class="collection-item" id="item-${item.id}">
                     <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
                     <a href="#" class="secondary-content">
-                        <i class="fa fa-edit"></i>
+                        <i class="edit-item fa fa-edit"></i>
                     </a>
                 </li>
                 `;
             });
             listContainer.innerHTML = html;
+        },
+
+        getUISelectors: function() {
+            return UISelectors;
+        },
+
+        getformInput: function() {
+            return {
+                name: document.querySelector(UISelectors.nameInput).value.trim(),
+                calories: document.querySelector(UISelectors.caloriesInput).value.trim()
+            };
+        },
+
+        addListItem: function(item) {
+            // Create li elment
+            const li = document.createElement('li');
+            li.className = 'collection-item';
+            li.setAttribute('id', `item-${item.id}`);
+            li.innerHTML = `
+                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content">
+                    <i class="edit-item fa fa-edit"></i>
+                </a>
+            `;
+
+            const listContainer = document.querySelector(UISelectors.itemList);
+            listContainer.appendChild(li);
+            console.log('Done');
+
         }
     };
 })();
 
 // App Ctrl
 const App = (function(ItemCtrl, UICtrl) {
+
+    // Load event listeners
+    const loadEventListeners = function() {
+        const UISelectors = UICtrl.getUISelectors();
+        
+        // Add item event
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+    };
+
+    const itemAddSubmit = function(e) {
+        e.preventDefault();
+        
+        // Get form input from UICtrl
+        const input = UICtrl.getformInput();
+        
+        // Validate name and calories
+        if ( input.name == '' || input.calories == '' ) { return false; }
+
+        // AddItem
+        const newItem = ItemCtrl.addItem(input.name, input.calories);
+
+        // Add Item to UI List
+        UICtrl.addListItem(newItem);
+    };
     
     // PUBLIC METHODS
     return {
@@ -70,6 +150,8 @@ const App = (function(ItemCtrl, UICtrl) {
             // Populate list with items
             UICtrl.populateItemList(items);
 
+            // Load Event listeners
+            loadEventListeners();
         }
     };
 
